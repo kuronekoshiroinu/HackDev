cfe6f0d6034195f940faab4beaf4cac6165c97cb
Jimbo <dextron.js@gmail.com>
2024-04-14 09:55:32 -0400

    iniciando

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+import 'tailwindcss/tailwind.css'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className="text-green-900 font-extrabold">Empezando</div>
+      
+    </>
+    // <BrowserRouter>   
+    // <Routes>
+    //   <Route path="/" element={<Navigate to ="/home"/>} />
+    //   <Route path="/home" element={<Pag1/>}/>
+    //   <Route path="/algo1" element={<pag2/>}/>
+    //   <Route path="/algo2" element={<pag3/>}/>
+    // </Routes>
+    // </BrowserRouter>  
+  )
+}
+
+export default App
+
+
