05387a97a7ee8040f8e68d889e86729563c4222f
Jimbo <jimbo.gate47@gmail.com>
2024-04-19 20:27:30 -0400

    updated:card flip

--- src/components/Board/BoardRow.jsx
+++ src/components/Board/BoardRow.jsx
@@ -1,11 +1,36 @@
 import { Card } from "./Card";
 
+const cardDirection = [
+    { direction: 0 },
+    { direction: 180 },
+    { direction: -90 },
+    { direction: 90 }
+]
+
+function cardBuilder(ncards) {
+    let cardstype = []
+    for (let i = 0; i < ncards; i++) {
+        cardstype.push({
+            key: i,
+            direction: cardDirection[Math.floor(Math.random() * 4)].direction
+        })
+    }
+    return cardstype
+}
+
 export function BoardRow() {
+    // cardBuilder se ejecuta para cada BoardRow
     return (
         <div className="grid grid-cols-3 gap-4">
-            <Card />
-            <Card />
-            <Card />
+            {
+                cardBuilder(3).map(
+                    (cardtype) =>
+                        <Card
+                            key={cardtype.key}
+                            direction={cardtype.direction}
+                        />
+                )
+            }
         </div>
     )
 }
--- src/components/Board/Card.jsx
+++ src/components/Board/Card.jsx
@@ -1,18 +1,40 @@
 import { useState } from "react";
+import redarrow from "../../assets/redarrow.svg"
+import inter from "../../assets/inter.svg"
 
-export function Card() {
+function frontCard(direction) {
+    let rotation = ""
+    if (direction == 90) rotation="rotate-90"
+    else if (direction == 180) rotation="rotate-180"
+    else if (direction == -90) rotation="-rotate-90"
+    let className = `shadow-md h-20 w-20 ${rotation}`
+
+    return (
+        <div className={className}>
+            <img src={redarrow}></img>
+        </div>
+    )
+}
+
+function reverseCard() {
+    return (
+        <div className="shadow-md h-20 w-20 hover:w-24">
+            <img src={inter}></img>
+        </div>
+    )
+}
+
+export function Card({direction}) {
     const [reverse, setReverse] = useState(true);
     const onClick = function () {
         reverse ? setReverse(false) : setReverse(true);
     }
 
     return (
-        <div className="hover:shadow-md cursor-alias" onClick={onClick}>
+        <div onClick={onClick}>
             {
-                reverse ? <div> Carta-Reverso</div> : <div> Carta-Frente</div>
+                reverse ? reverseCard() : frontCard(direction)
             }
-        </div>
+        </div >
     )
-
-
 }
