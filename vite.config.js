3f44436dcb0fcb0d8abcd70885275d48afb121c6
Paola <pvillanueva.gece@gmail.com>
2024-04-26 22:42:51 -0400

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import removeConsole from 'vite-plugin-remove-console';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), removeConsole()],
+  base: "https://kuronekoshiroinu.github.io/HackDev"
 })
